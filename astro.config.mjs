4cb717cbdd35406a0883f02ed9be82447787bad6
Victor Lebkowski <vlebkowski@cmimedia.fr>
2025-12-12 00:28:58 +0100

    critical css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,31 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import { VitePWA } from 'vite-plugin-pwa';
+import { generate } from 'critical';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [
+    {
+      name: 'critical-css',
+      hooks: {
+        'astro:build:done': async ({ dir }) => {
+            await generate({
+                base: './dist/',
+                src: 'index.html',
+                target: 'index.html',
+                inline: true,
+                extract: true,
+                dimensions: [
+                    { width: 375, height: 600 },
+                    { width: 1300, height: 900 }
+                ]
+            });
+            console.log('✅ Critical CSS generated (Astro Integration)');
+        }
+      }
+    }
+  ],
   vite: {
     plugins: [
       VitePWA({
