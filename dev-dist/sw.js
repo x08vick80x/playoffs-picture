3e6dd291a9a6b7f0f487a3b3de57f435918f2c7e
Victor Lebkowski <vlebkowski@cmimedia.fr>
2025-12-16 23:18:02 +0100

    littles fixes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "/index.html",
-    "revision": "0.q9q6ha0vq6o"
+    "revision": "0.bibef25bo8"
   }], {
     "directoryIndex": "index.html"
   });
